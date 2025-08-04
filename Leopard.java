928d0482ca7a34d57234c9e888e1b83ffbdf1e2e
baosenchen <bac009@ucsd.edu>
2025-08-03 21:19:49 -0700

    Update Leopard.java

--- Leopard.java
+++ Leopard.java
@@ -1,38 +1,30 @@
 import java.awt.Color;
 
 public class Leopard extends Critter {
-    private int moveCount;
-
     public Leopard() {
-        super("L");
-        moveCount = 0;
-    }
-
-    @Override
-    public Direction getMove() {
-        moveCount++;
-        if (moveCount % 2 == 0) {
-            return Direction.EAST;
-        } else {
-            return Direction.NORTH;
-        }
-    }
-
-    @Override
-    public boolean eat() {
-        return Math.random() < 0.5;
-    }
-
-    @Override
-    public Attack getAttack(String opponent) {
-        if (opponent.equals("T")) {
-            return Attack.SCRATCH;
-        }
-        return Attack.ROAR;
+        super("Leo");
     }
 
     @Override
     public Color getColor() {
-        return new Color(210, 180, 140);
+        return Color.YELLOW;
+    }
+
+    @Override
+    public Critter.Attack getAttack(String opponent) {
+        if (opponent.equals("T")) {
+            return Critter.Attack.ROAR;
+        }
+        return Critter.Attack.POUNCE;
+    }
+
+    @Override
+    public Direction getMove() {
+        return Direction.values()[random.nextInt(4)];
+    }
+
+    @Override
+    public boolean eat() {
+        return random.nextBoolean();
     }
 }
