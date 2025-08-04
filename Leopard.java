4d718124eb7a4dfaa9c4126ba7a8fe0a2a86b0ad
baosenchen <bac009@ucsd.edu>
2025-08-03 20:58:57 -0700

    Update Leopard.java

--- Leopard.java
+++ Leopard.java
@@ -1,81 +1,39 @@
 import java.awt.Color;
 
-public class Leopard extends Feline {
-    protected static int confidence = 0;
+public class Leopard extends Critter {
+    private int stepCount;
 
     public Leopard() {
-        super();
-        this.displayName = "Lpd";
-    }
-
-    @Override
-    public Color getColor() {
-        return Color.RED;
+        super("L");
+        stepCount = 0;
     }
 
     @Override
     public Direction getMove() {
-        String north = info.getNeighbor(Direction.NORTH);
-        String south = info.getNeighbor(Direction.SOUTH);
-        String east = info.getNeighbor(Direction.EAST);
-        String west = info.getNeighbor(Direction.WEST);
-
-        if (north.equals(".") || north.equals("Patrick")) {
-            return Direction.NORTH;
-        } else if (south.equals(".") || south.equals("Patrick")) {
-            return Direction.SOUTH;
-        } else if (east.equals(".") || east.equals("Patrick")) {
-            return Direction.EAST;
-        } else if (west.equals(".") || west.equals("Patrick")) {
-            return Direction.WEST;
-        } else {
-            // 
-            Direction[] dirs = {Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST};
-            return dirs[random.nextInt(dirs.length)];
+        stepCount++;
+        if (stepCount % 3 == 0) {
+            return Direction.values()[(int)(Math.random() * 4)];
         }
+        return Direction.NORTH;
     }
 
     @Override
     public boolean eat() {
-        int chance = confidence * 10;
-        return random.nextInt(100) < chance;
-    }
-
-    @Override
-    public void win() {
-        if (confidence < 10) {
-            confidence++;
-        }
-    }
-
-    @Override
-    public void lose() {
-        if (confidence > 0) {
-            confidence--;
-        }
+        
+        return Math.random() < 0.5;
     }
 
     @Override
     public Attack getAttack(String opponent) {
-        if (opponent.equals("Turtle") || confidence > 5) {
-            return Attack.POUNCE;
-        } else {
-            return generateAttack(opponent);
+        
+        if (opponent.equals("%") || opponent.equals("S") || opponent.equals("T")) {
+            return Attack.ROAR;
         }
-    }
-
-    // required protected helper
-    protected Attack generateAttack(String opponent) {
-        if (opponent.equals("Patrick")) {
-            return Attack.FORFEIT;
-        }
-
-        Attack[] attacks = {Attack.ROAR, Attack.POUNCE, Attack.SCRATCH};
-        return attacks[random.nextInt(attacks.length)];
+        return Attack.SCRATCH;
     }
 
     @Override
-    public void reset() {
-        confidence = 0;
+    public Color getColor() {
+        return new Color(210, 180, 140); 
     }
 }
