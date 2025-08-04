c2d225076376504abe1bd3242f154f08361ac7c7
baosenchen <bac009@ucsd.edu>
2025-08-03 21:24:59 -0700

    Update Ocelot.java

--- Ocelot.java
+++ Ocelot.java
@@ -1,30 +1,39 @@
-import java.awt.Color;
+import java.util.Random;
 
 public class Ocelot extends Critter {
+    private Random rand = new Random();
+
     public Ocelot() {
-        super("Oce");
+        super("Ocelot");
     }
 
-    @Override
-    public Color getColor() {
-        return Color.ORANGE;
-    }
-
-    @Override
-    public Critter.Attack getAttack(String opponent) {
-        if (opponent.equals("L")) {
-            return Critter.Attack.FORFEIT;
-        }
-        return Critter.Attack.SCRATCH;
-    }
-
-    @Override
     public Direction getMove() {
-        return Direction.values()[random.nextInt(4)];
+        int n = rand.nextInt(8);
+        switch (n) {
+            case 0: return Direction.NORTH;
+            case 1: return Direction.SOUTH;
+            case 2: return Direction.EAST;
+            case 3: return Direction.WEST;
+            case 4: return Direction.NORTHEAST;
+            case 5: return Direction.NORTHWEST;
+            case 6: return Direction.SOUTHEAST;
+            default: return Direction.SOUTHWEST;
+        }
     }
 
-    @Override
+    public Attack getAttack(String opponent) {
+        if (opponent.equals("F")) {
+            return Attack.SCRATCH;
+        }
+        return Attack.POUNCE;
+    }
+
     public boolean eat() {
-        return true;
+        return rand.nextBoolean();
+    }
+
+    public Color getColor() {
+        return Color.MAGENTA;
     }
 }
+
